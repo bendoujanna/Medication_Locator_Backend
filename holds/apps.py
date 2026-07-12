6da99550941e0cbb0e50eae9ea6d1e17841d39b1
bendoujanna <b.vitalina@alustudent.com>
2026-07-12 13:58:14 +0200

    chore(holds): initialize background scheduler on app startup

--- holds/apps.py
+++ holds/apps.py
@@ -1,12 +1,16 @@
 from django.apps import AppConfig
-import os
+
 
 class HoldsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "holds"
 
     def ready(self):
-
+        """
+        Start the background scheduler when Django finishes booting.
+        The scheduler runs expire_stale_holds() every 5 minutes.
+        """
+        import os
         if os.environ.get("RUN_MAIN") != "true":
             from holds.tasks import start_scheduler
             start_scheduler()
