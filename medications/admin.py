912858de760dde12dc91d672ec817de213bfe32a
bendoujanna <b.vitalina@alustudent.com>
2026-07-13 05:25:51 +0200

    feat(medications): configure django admin for drug catalog
    - Register ActiveIngredient and Medication models
    - Enable autocomplete_fields to optimize admin UI for large medicine databases
    - Configure dynamic list filters by dosage form and symptom category

--- medications/admin.py
+++ medications/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from medications.models import ActiveIngredient, Medication
 
-# Register your models here.
+
+@admin.register(ActiveIngredient)
+class ActiveIngredientAdmin(admin.ModelAdmin):
+    list_display = ["name", "symptom_category", "ems_reference_code"]
+    search_fields = ["name", "symptom_category", "ems_reference_code"]
+    readonly_fields = ["ingredient_id"]
+
+
+@admin.register(Medication)
+class MedicationAdmin(admin.ModelAdmin):
+    list_display = ["brand_name", "strength", "dosage_form", "ingredient"]
+    list_filter = ["dosage_form", "ingredient__symptom_category"]
+    search_fields = ["brand_name", "ingredient__name"]
+    readonly_fields = ["medication_id"]
+    autocomplete_fields = ["ingredient"]
